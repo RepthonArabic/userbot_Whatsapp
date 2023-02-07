6ddb76f5698ef6127bd5a25a6c850b50420c656b
روجر <104784913+rogerpq@users.noreply.github.com>
2023-02-07 19:28:03 +0300

    Update Telegraph.js

--- commands/Telegraph.js
+++ commands/Telegraph.js
@@ -21,7 +21,7 @@ cmd({
     },
     async(Void, citel,text) => {
  let a = citel.quoted ? citel.quoted.text : citel.text;
-let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=Secktor-Md+Bot&author_name=SamPandey001&content=[%7B"tag":"p","children":["${a.replace(/ /g,'+')}"]%7D]&return_content=true`);
+let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=userbot_Whtsapp+Bot&author_name=RepthonArabic&content=[%7B"tag":"p","children":["${a.replace(/ /g,'+')}"]%7D]&return_content=true`);
 return citel.reply(`*تم انشاء رابط التلكراف انقر لنسخه*\nالاسم:-${util.format(data.result.title)} \nالرابط:- ${util.format(data.result.url)}`)
     }
 );
